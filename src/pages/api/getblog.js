25217066f2d2dc216cd7ceef807af1ecc0ce3a42
rhi8 <72750646+rhi8@users.noreply.github.com>
2024-05-29 00:41:00 +0400

    api to get data added

---
+++ src/pages/api/getblog.js
@@ -0,0 +1,45 @@
+
+
+
+import { connectDatabase, insertDocument,getAllDocuments } from "@/helpers/db-util";
+
+
+
+async function handler(req, res) {
+    if (req.method === 'GET') {
+      const blog = req.body.blog;
+  
+     
+  
+      let client;
+  
+      try {
+        client = await connectDatabase();
+      } catch (error) {
+        res.status(500).json({ message: 'Connecting to the database failed!' });
+        return;
+      }
+
+      try {
+        const documents = await getAllDocuments(client, 'newblogs', { _id: -1 });
+        res.status(200).json({ blogs: documents });
+      } catch (error) {
+        res.status(500).json({ message: 'Getting comments failed.' });
+      }
+
+
+  
+     /*  try {
+        await insertDocument(client, 'newblogs', { blog: blog });
+        client.close();
+      } catch (error) {
+        res.status(500).json({ message: 'Inserting data failed!' });
+        return;
+      }
+  
+      res.status(201).json({ message: 'blog added' }); */
+    }
+  }
+  
+  export default handler;
+  
